fd4d643962607081aa01644bd8ebe71bb4cfb466
HealozNoob <89713262+HealozNoob@users.noreply.github.com>
2021-09-01 13:30:44 +1000

    Creating SaveFile header file

---
+++ LaurenEasterA2/SaveFile.h
@@ -0,0 +1,54 @@
+/*
+Project: Assignment 2 - Hunt the Wumpus Prototype
+Author: Lauren Easter
+Student ID: 30598079
+*/
+
+#ifndef SAVEFILE_H
+#define SAVEFILE_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include "Enums.h"
+#include "Location.h"
+using namespace std;
+
+// Constants
+
+const int MAX_SAVEFILES = 15;
+
+// CLASS DEFINITION
+
+class SaveFile {
+
+	// public variables
+public:
+
+	// Constructors
+
+	SaveFile();
+	SaveFile(string saveFileName); //overloading constructor
+	~SaveFile();
+
+	// accessor methods
+	string getSaveFileName();
+
+
+	// mutator methods
+	void setSaveFileName(string name);
+
+private:
+
+	// private variables
+	string locationName;
+	vector<string> locationExits;
+	string locationDescription;
+};
+
+#endif
+
